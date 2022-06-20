68190f8ef6144872020d2fa111e8043fdc959179
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-06-20 04:24:31 +0200

    feat: add solidjs-tailwind template

---
+++ solidjs-tailwind/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div class="flex justify-center items-center h-screen text-3xl">
+      Hello world
+    </div>
+  );
+};
+
+export default App;
